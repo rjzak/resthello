de73781219d941e8faa9e034c0d82bbfbaeca401
Richard Zak <richard.j.zak@gmail.com>
2022-01-14 10:13:29 -0500

    Adding versioned API, client uses `structopt`, greet user
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,38 @@
+use std::borrow::Borrow;
 use std::error::Error;
+
+use structopt::StructOpt;
+use serde::{Serialize, Deserialize};
+
+#[derive(StructOpt, Debug, Serialize, Deserialize)]
+#[structopt(name = "client")]
+struct ClientOptions {
+    // The IP address to bind to
+    #[structopt(name="name", short, long)]
+    name: String
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let body = reqwest::get("http://localhost:3000").await?.text().await?;
+    let opt = ClientOptions::from_args();
+    let mut greeting_post:String = "{\"name\":\"".to_string();
+    greeting_post.push_str(&opt.name);
+    greeting_post.push_str("\"}");
 
-    println!("{}", body);
+    //let body = reqwest::get("http://localhost:3000").await?.text().await?;
+    //let body = reqwest::post("http://localhost:3000/v1/greet").body(greetingPost).await?.text().await?;
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .post("http://localhost:3000/v1/greet")
+        .header("Content-Type", "application/json")
+        .json(&opt)
+        .send()
+        .await?;
+
+    let body = resp.bytes().await?;
+
+
+    println!("{}", String::from_utf8_lossy(body.borrow()).to_string());
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,19 @@
 use std::net::SocketAddr;
 
-use axum::routing::get;
+use axum::routing::{get, post};
 use axum::Router;
+use axum::extract;
+use axum::Json;
+use serde::Deserialize;
+use serde_json::{Value, json};
+#[derive(Deserialize)]
+struct Greeting {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(root)).route("/v1/greet", post(greet_person));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
@@ -16,4 +24,11 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn greet_person(payload: extract::Json<Greeting>) -> String {
+    let payload: Greeting = payload.0;
+    let mut temp_string:String = "hello, ".to_string();
+    temp_string.push_str(&payload.name.to_owned());
+    temp_string
 }
